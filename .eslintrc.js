ba8fa19af10debabc9efb8d6ca9ec0030f918987
javier olayis <jolaya182@gmail.com>
2018-01-15 15:30:07 -0500

    addedLinter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins":[
+        "import",
+    ],
+    "rules": {
+        "react/jsx-filenname-extension" :0,
+        "no-alert": 0,
+        "quotes": 0,
+        "no-use-before-define": 0,
+        "linebreak-style":0,
+        "no-undef": 0
+
+
+    }
+
+        
+    
+};
--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.use(bodyParser.urlencoded({extended: true}) );
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/api", api);
 
-app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "dist/index/.html")); }  );
+app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "dist/index/.html")); });
 
-app.listen( port, function(){
+app.listen(port, function () {
 
-	console.log("server running on localhost:"+ port);
+	console.log("server running on localhost:" + port);
 
-} )
+})
